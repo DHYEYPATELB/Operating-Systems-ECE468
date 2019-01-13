6ea38294a931f7e93f176889ec5f37b7ed97103c
ennth <39110859+aemaeth0@users.noreply.github.com>
2019-01-12 20:48:38 -0600

    formatted input output stream data test Java

--- testingPassByRefVal/src/testingPassByRefVal/testingPassByRefVal.java
+++ testingPassByRefVal/src/testingPassByRefVal/testingPassByRefVal.java
@@ -4,9 +4,22 @@ public class testingPassByRefVal {
 	
 	
 	public static void main(String[] args) {
+		int rows = 3;
+		int cols = 5;
 		
-		int x = 3;
-		int y = 4;
+		for(int i = 0; i<rows; i++) {
+			// creates 3 rows
+			
+			for(int j = 0; j<cols; j++) {
+				System.out.print("-");
+				
+			}
+			System.out.println();
+			
+			
+			
+			
+		}
 		
 		
 		
