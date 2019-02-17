054c8de3656e1ac12038f9d31a51e7613b63ba15
ennth <39110859+aemaeth0@users.noreply.github.com>
2019-02-16 21:53:49 -0600

    defining command line

--- Assignment 2/launcher.c
+++ Assignment 2/launcher.c
@@ -1,3 +1,20 @@
 #include <stdio.h> // scanf(), fscanf(), fprintf(), fopen()
 #include <windows.h>
 
+int main () {
+
+
+
+ /* set up the command lines */
+   lpCommandLine[0] = "C:\\Windows\\notepad.exe"; // notepad
+   lpCommandLine[1] = "C:\\Program Files\\windows nt\\accessories"; // wordpad
+   lpCommandLine[2] = "C:\\Windows\\System32\\nslookup.exe"; // cmd.exe
+   lpCommandLine[3] = "C:\\Windows\\system32\\calc.exe"; // calc.exe
+   lpCommandLine[4] = "C:\\Windows\\System32\\nslookup.exe"; // explorer.exe
+
+
+
+}
+
+
+
