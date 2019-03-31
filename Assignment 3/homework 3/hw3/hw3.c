adcf0c19533aa63c21730319af196be364b4c0d0
ennth <39110859+aemaeth0@users.noreply.github.com>
2019-03-31 01:13:21 -0500

    added GetProcessAffinity mask and looping thru command line args

--- Assignment 3/homework 3/hw3/hw3.c
+++ Assignment 3/homework 3/hw3/hw3.c
@@ -1,12 +1,9 @@
 /*
-Alberto Pizano
-Assignment 3
-Operating Systems 468
-3/30/2019
+
+
 */
 #include <windows.h>
 #include <stdio.h>
-
 
 typedef struct processor_data {
    int affinityMask;                /* affinity mask of this processor (just one bit set) */
@@ -18,16 +15,12 @@ typedef struct processor_data {
 /* function prototypes */
 void printError(char* functionName);
 
-
-
 int main(int argc, char *argv[])
 {
    int processorCount = 0;       /* the number of allocated processors */
    ProcessorData *processorPool; /* an array of ProcessorData structures */
    HANDLE *processHandles;       /* an array of handles to processes */
-
-
-
+   
    if (argc < 3)
    {
       fprintf(stderr, "usage, %s  SCHEDULE_TYPE  SECONDS...\n", argv[0]);
@@ -39,13 +32,54 @@ int main(int argc, char *argv[])
 
 
    /* read the job duration times off the command-line */
+   int cmdLineArgsArray[argc];
+   
+   // To print out Length of command line args array
+   
+    //while (argc != 0) {
+    //printf("argument %d \n", argc);
+    //argc--;
+    //}
+    
+    // To print out elements values of command line args array
+    for(int i=0; i<argc; i++) {
+    printf(argv[i]);
+    printf(" ");
+    }
+    printf("\n");
+    
+    int index = 0;
+    int count = 0;
+    
+    for(int i=1; i<argc; i++) {
+    cmdLineArgsArray[index++] = argv[i];
+    count++;
+    }
+    
+    
+    for(int i=0; i<count; i++) {
+    printf("The value for arg %d is %s \n", i,cmdLineArgsArray[i]);
+    }
+  
 
    /* get the processor affinity mask for this process */
-   GetProcessAffinityMask(
-   HANDLE     hProcess,
-   PDWORD_PTR lpProcessAffinityMask,
-   PDWORD_PTR lpSystemAffinityMask
-   );
+   ProcessorData processor_data;
+   
+   DWORD_PTR lpProcessAffinityMask = 0;
+   DWORD_PTR lpSystemAffinityMask = 0;
+   
+   HANDLE process = GetCurrentProcess();
+
+   BOOL res = GetProcessAffinityMask(GetCurrentProcess(), &lpProcessAffinityMask, &lpSystemAffinityMask);
+   
+      printf("%d 0x%X 0x%X \n",
+            res,
+            lpProcessAffinityMask,
+            lpSystemAffinityMask);
+            
+   processor_data.running = GetProcessAffinityMask(processor_data.processInfo.hProcess, (PDWORD_PTR)&processor_data.affinityMask, &lpSystemAffinityMask);
+   
+      printf("%d 0x%X 0x%X \n",processor_data.running,processor_data.affinityMask,lpSystemAffinityMask);
 
    /* count the number of processors set in the affinity mask */
 
@@ -67,8 +101,8 @@ int main(int argc, char *argv[])
       /* check that there are still processes running, if not, quit */
 
       /* wait for one of the running processes to end */
-      if (WAIT_FAILED == (result = WaitForMultipleObjects(handleCount, processHandles, FALSE, INFINITE)))
-         printError("WaitForMultipleObjects");
+      //if (WAIT_FAILED == (result = WaitForMultipleObjects(handleCount, processHandles, FALSE, INFINITE)))
+         //printError("WaitForMultipleObjects");
 
       /* translate result from an index in processHandles[] to an index in processorPool[] */
 
